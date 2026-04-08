6510118dba7e9cc0d096559982d916c51235da99
Inrixia <6373693+Inrixia@users.noreply.github.com>
2026-04-08 20:13:23 +0000

    nix: update Darwin injection and package hashes for 1.13.0-beta

--- nix/injection-darwin.nix
+++ nix/injection-darwin.nix
@@ -21,7 +21,7 @@ in
     pnpmDeps = fetchPnpmDeps {
       inherit pname src version;
       fetcherVersion = 1;
-      hash = "sha256-pHIY4Ie66ZVwEne/4RmY2QvsRWcnfsl2kv3CDXcqVrg=";
+      hash = "sha256-n3S7IzTTCoJdA80lmy5mQ2RJ7fj1EF7nk+oBcGTwYRM=";
     };
 
     buildPhase = ''
